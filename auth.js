44b499e79bea629a6755db9daf4e5ce3859bae48
Sara <sara.szajewska@gmail.com>
2017-03-28 15:23:06 +0200

    changes

--- auth.js
+++ auth.js
@@ -3,6 +3,7 @@ window.onload = function(){ "use strict";
 let signin = document.getElementById("signin");
 let signout = document.getElementById("signout");
 let status = document.getElementById("status");
+let image = document.getElementById("divImage");
                          
 let provider = new firebase.auth.GithubAuthProvider();
                              
@@ -11,11 +12,33 @@ signin.addEventListener("click", function(event){
 firebase.auth().signInWithPopup(provider)
     .then((result)=>{
         console.log(result);
-        let user = result.user;
+    
+        if(result.user != null){
+            window.sessionStorage.user = JSON.stringify(result.user);
+            let user = sessionStorage.user;
+        if(user === undefined || user === null){
+            signout.disabled = true;
+            }
+        }
+    else {
+        user = JSON.parse(user);
+        status.innerHTML = user.displayName;
+        image.setAttribute("src", user.photoURL);
+    }
     })
     .catch((error)=>{
         console.log(error);
     });
 })
+
+// Logga ut den autentiserade användaren
+firebase.auth().signOut()
+.then(function(result) {
+    console.log(result);
+   
+})
+.catch(function(error) {
+    console.log(error);
+});
 
 }
