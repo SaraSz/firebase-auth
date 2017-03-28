e9a20619dac11472022e0d3d23ae7807659046f1
Sara <sara.szajewska@gmail.com>
2017-03-28 15:54:22 +0200

    bracket

--- auth.js
+++ auth.js
@@ -2,30 +2,31 @@ window.onload = function(){ "use strict";
                            
 let signin = document.getElementById("signin");
 let signout = document.getElementById("signout");
-
-                         
+                        
 let provider = new firebase.auth.GithubAuthProvider();
                              
                              
 signin.addEventListener("click", function(event){
-firebase.auth().signInWithPopup(provider)
+    firebase.auth().signInWithPopup(provider)
     .then((result)=>{
         console.log(result);
-    
+     let user = sessionStorage.user;
         if(result.user != null){
             window.sessionStorage.user = JSON.stringify(result.user);
-            let user = sessionStorage.user;
+            
         if(user === undefined || user === null){
             signout.disabled = true;
             }
-        }
+        
     else {
+       
         user = JSON.parse(user);
         let image = document.getElementById("divImage");
         let status = document.getElementById("status");
         status.innerHTML = user.displayName;
         image.setAttribute("src", user.photoURL);
     }
+        }
     })
     .catch((error)=>{
         console.log(error);
