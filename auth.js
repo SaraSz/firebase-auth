58fb05676e2e0b2fea926d3a26a5c1e2bd73bef4
Sara <sara.szajewska@gmail.com>
2017-03-30 10:28:55 +0200

    test

--- auth.js
+++ auth.js
@@ -3,22 +3,25 @@ window.onload = function(){ "use strict";
 let signin = document.getElementById("signin");
 let signout = document.getElementById("signout");
 let image = document.getElementById("divImage");   
-                           
+let specialBtn = document.getElementById("specialBtn");                           
 let provider = new firebase.auth.GithubAuthProvider();
                              
-console.log("Innan event.");                             
+console.log("Innan event.");
+                           
 signin.addEventListener("click", function(event){
     firebase.auth().signInWithPopup(provider)
     .then((result)=>{
-        console.log("Vi testar Sara: " + result.user);
+        console.log("Vi testar användare: " + result.user);
      
         if(result.user != null){
+            
+            signin.disabled = true;
             
             let user = JSON.stringify(result.user);
             let data = JSON.parse(user);    
             
             let status = document.getElementById("status");
-            status.innerHTML = data.displayName;
+            status.innerHTML = "You are logged in as: " data.displayName;
 
             //image.setAttribute("src", data.photoURL);
             localStorage.setItem("imageURL", data.photoURL);
@@ -41,7 +44,7 @@ signin.addEventListener("click", function(event){
 signout.addEventListener("click", function(event){
     firebase.auth().signOut()
         .then(function(result) {
-    delete sessionStorage.user;
+    localStorage.removeItem("user", user);
     status.innerHTML = "You are logged out!";
     console.log(result);
    
